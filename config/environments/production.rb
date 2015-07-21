0931534333d4bec2dfc429ddf3db3c39bbb19ae2
Michael Cheng <mcheng.work@gmail.com>
2015-07-22 02:05:46 +0800

    Add base mode to env variables.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    ActiveMerchant::Billing::Base.mode = :production
+    ActiveMerchant::Billing::Base.mode = ENV["PAYPAL_API_MODE"].to_sym
     paypal_options = {
       login: ENV["PAYPAL_API_USERNAME"],
       password: ENV["PAYPAL_API_PASSWORD"],
