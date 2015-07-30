50657ab097e63077b9cb1e9b1b73cb82dec99354
Michael Cheng <mcheng.work@gmail.com>
2015-07-30 23:17:40 +0800

    Add bcc recipient.

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -3,7 +3,7 @@ class OrdersMailer < ApplicationMailer
     @purchase_order = purchase_order
     @event = purchase_order.event
 
-    mail(to: @purchase_order.payer_email, subject: "[#{@event.name}] Ticket Purchase Order - #{@purchase_order.invoice_no} - #{@purchase_order.purchased_at.strftime('%e-%b-%Y')}")
+    mail(to: @purchase_order.payer_email, bcc: 'orders@phpconf.asia', subject: "[#{@event.name}] Ticket Purchase Order - #{@purchase_order.invoice_no} - #{@purchase_order.purchased_at.strftime('%e-%b-%Y')}")
   end
 
   def attendee_notification(purchase_order, ticket)
@@ -15,6 +15,6 @@ class OrdersMailer < ApplicationMailer
     logo_path = File.join(Rails.root, 'app', 'assets', 'images', 'krgh_facade_day.png')
     attachments.inline['krgh_facade_day.png'] = File.read(logo_path)
 
-    mail(to: @ticket.attendee.email, subject: "[#{@event.name}] Event Ticket (#{@ticket.order.ticket_type.name}) - #{@purchase_order.purchased_at.strftime('%e-%b-%Y')}")
+    mail(to: @ticket.attendee.email, bcc: 'orders@phpconf.asia', subject: "[#{@event.name}] Event Ticket (#{@ticket.order.ticket_type.name}) - #{@purchase_order.purchased_at.strftime('%e-%b-%Y')}")
   end
 end
